5193dd38e559d2bb162ffcfda4bbe28f9452a75f
geoom <hi.geoom@gmail.com>
2019-04-28 03:29:53 -0500

    add login and signup features

---
+++ api/app/serializers/user_serializer.rb
@@ -0,0 +1,7 @@
+class UserSerializer < ActiveModel::Serializer
+  
+  attributes :id, :name, :email
+
+  has_many :items
+
+end
--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_28_062321) do
+ActiveRecord::Schema.define(version: 2019_04_28_075417) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,14 @@ ActiveRecord::Schema.define(version: 2019_04_28_062321) do
     t.string "title"
     t.text "content"
     t.string "created_by"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
