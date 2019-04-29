4cc6d384bfb71a2087dcdafdb224d58aeefbfae1
geoom <hi.geoom@gmail.com>
2019-04-29 11:07:08 -0500

    add login and signup container

---
+++ web/src/actions/session.js
@@ -0,0 +1,45 @@
+import { reset } from 'redux-form';
+import api from '../api';
+
+function setCurrentUser(dispatch, response) {
+  localStorage.setItem('token', JSON.stringify(response.meta.auth_token));
+  dispatch({ type: 'AUTHENTICATION_SUCCESS', response });
+}
+
+export function login(data, router) {
+  return dispatch => api.post('/auth/login', data)
+    .then((response) => {
+      setCurrentUser(dispatch, response);
+      dispatch(reset('login'));
+      router.transitionTo('/');
+    });
+}
+
+export function signup(data, router) {
+  return dispatch => api.post('/signup', data)
+    .then((response) => {
+      setCurrentUser(dispatch, response);
+      dispatch(reset('signup'));
+      router.transitionTo('/');
+    });
+}
+
+export function logout(router) {
+  return dispatch => api.delete('/auth')
+      .then(() => {
+        localStorage.removeItem('token');
+        dispatch({ type: 'LOGOUT' });
+        router.transitionTo('/login');
+      });
+}
+
+export function authenticate() {
+  return dispatch => api.post('/auth/refresh')
+      .then((response) => {
+        setCurrentUser(dispatch, response);
+      })
+      .catch(() => {
+        localStorage.removeItem('token');
+        window.location = '/login';
+      });
+}
--- web/src/containers/App/index.js
+++ web/src/containers/App/index.js
@@ -1,20 +1,46 @@
 // @flow
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { authenticate } from '../../actions/session';
 import Home from '../Home';
 import NotFound from '../../components/NotFound';
+import Login from '../Login';
+import Signup from '../Signup';
+
+type Props = {
+  authenticate: () => void,
+}
 
 class App extends Component {
+
+  componentDidMount() {
+    const token = localStorage.getItem('token');
+
+    if (token) {
+      this.props.authenticate();
+    }
+  }
+
+  props: Props
+
   render() {
     return (
       <BrowserRouter>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route component={NotFound} />
-        </Switch>
+        <div style={{ display: 'flex', flex: '1' }}>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/login" component={Login} />
+            <Route path="/signup" component={Signup} />
+            <Route component={NotFound} />
+          </Switch>
+        </div>
       </BrowserRouter>
     );
   }
 }
 
-export default App;
+export default connect(
+  null,
+  { authenticate }
+)(App);
